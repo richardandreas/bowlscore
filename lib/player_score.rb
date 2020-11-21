5e2afce58248a3f4d440760fe6384980db40d035
Richard Andreas Stierlin <@61023391341>
2020-11-21 19:49:57 -0300

    feature: data validation

--- lib/player_score.rb
+++ lib/player_score.rb
@@ -1,15 +1,20 @@
 # Initializes with an array of rolls from a single player and calculates the scores of the full
 # game.
 class PlayerScore
-  attr_reader :rolls, :scores, :total_score
+  attr_reader :errors, :rolls, :scores, :total_score, :valid
 
   def initialize(rolls)
-    @rolls = rolls
+    @rolls  = rolls
+    @errors = []
 
-    calculate
+    validate_positive_numbers
+    validate_tenth_frame
+
+    calculate if valid?
   end
 
   def valid?
+    !@errors.any?
   end
 
   private
@@ -34,6 +39,29 @@ class PlayerScore
 
       @scores << @total_score
     end
+
+  rescue TypeError => e
+    @errors << 'Number of rolls is too short'
+  end
+
+  # Checks if the rolls dataset is free of negative numbers
+  def validate_positive_numbers
+    @errors << 'Negative points are not allowed' if @rolls.any? { |n| n < 0 }
+  end
+
+  # Checks for the third roll in tenth frame with strike
+  def validate_tenth_frame
+    @rolls_count = @rolls.count + @rolls.count(10)
+
+    if @rolls_count == 21 && @rolls[-2] == 10
+      @errors << 'Tenth frame needs third roll'
+    elsif @rolls_count >= 21 && !third_roll_in_tenth_frame
+      @errors << 'Number of rolls is too long'
+    end
+  end
+
+  def third_roll_in_tenth_frame
+    @rolls_count == 22 && @rolls[-3] == 10
   end
 
   def next_points
