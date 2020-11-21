d7749dc134c6c741526bc349eca193a506af87b5
Richard Andreas Stierlin <@61023391341>
2020-11-21 16:12:01 -0300

    test: parsing player score files

---
+++ spec/file_parsers/bowlscore_file_parser_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe BowlscoreFileParser do
+  let(:path_to_file) { './spec/support/test-input-1.txt' }
+
+  describe '#parse' do
+    subject { BowlscoreFileParser.parse(path_to_file) }
+    it { expect(subject).to be_a Array }
+    it { expect(subject.length).to eq 35 }
+    it { expect(subject.first).to eq ['Jeff', 10] }
+    it { expect(subject.last).to eq ['John', 0] }
+  end
+end
