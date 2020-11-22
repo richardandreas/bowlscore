f75f19b7cb2ef43d9d84dba51f32f66dba398c88
Richard Andreas Stierlin <@61023391341>
2020-11-22 19:41:21 -0300

    test: further testing

--- spec/bowlscore_spec.rb
+++ spec/bowlscore_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe Bowlscore do
   let(:jeff_scores) { "Score\t\t20\t\t39\t\t48\t\t66\t\t74\t\t84\t\t90\t\t120\t\t148\t\t167\n" }
   let(:john_pinfalls) { "Pinfalls\t3\t/\t6\t3\t\tX\t8\t1\t\tX\t\tX\t9\t0\t7\t/\t4\t4\tX\t9\t0\n" }
   let(:john_scores) { "Score\t\t16\t\t25\t\t44\t\t53\t\t82\t\t101\t\t110\t\t124\t\t132\t\t151\n" }
+  let(:carl_pinfalls) { "Pinfalls\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\tX\tX\tX\n" }
+  let(:carl_scores) { "Score\t\t30\t\t60\t\t90\t\t120\t\t150\t\t180\t\t210\t\t240\t\t270\t\t300\n" }
 
   describe '#start' do
     it { expect { Bowlscore.start('./spec/support/test-input-1.txt') }
          .to output(header + "Jeff\n" + jeff_pinfalls + jeff_scores +
                              "John\n" + john_pinfalls + john_scores).to_stdout }
+
+    it { expect { Bowlscore.start('./spec/support/test-input-2.txt') }
+    .to output(header + "Carl\n" + carl_pinfalls + carl_scores).to_stdout }
   end
 end
--- spec/score_table_logger_spec.rb
+++ spec/score_table_logger_spec.rb
@@ -1,26 +1,46 @@
 RSpec.describe ScoreTableLogger do
   let(:player_rolls) { ['10', '7', '3', '9', '0', '10', '0', '8', '8', '2', 'F', '6', '10', '10', '10', '8', '1'] }
+  let(:all_fouls_rolls) { Array.new(20, 'F') }
+
   let(:player_frames) { PlayerScore.new(player_rolls).frames }
-  let(:logger) { ScoreTableLogger.new(player_frames, 'Jeff') }
+  let(:all_fouls_frames) { PlayerScore.new(all_fouls_rolls).frames }
+
+  let(:player_logger) { ScoreTableLogger.new(player_frames, 'Jeff') }
+  let(:all_fouls_logger) { ScoreTableLogger.new(all_fouls_frames, 'John') }
 
   let(:header) { "Frames\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10\n" }
   let(:pinfalls) { "Pinfalls\t\tX\t7\t/\t9\t0\t\tX\t0\t8\t8\t/\tF\t6\t\tX\t\tX\tX\t8\t1\n" }
   let(:scores) { "Score\t\t20\t\t39\t\t48\t\t66\t\t74\t\t84\t\t90\t\t120\t\t148\t\t167\n" }
+
+  let(:all_fouls_pinfalls) { "Pinfalls" + Array.new(20, "\tF").join + "\n" }
+  let(:all_fouls_scores) { "Score" + Array.new(10, "\t\t0").join + "\n" }
 
   describe '#print_header' do
     it { expect { ScoreTableLogger.print_header }.to output(header).to_stdout }
   end
 
   describe '#print_pinfalls' do
-    it { expect { logger.print_pinfalls }.to output(pinfalls).to_stdout }
+    context 'average game' do
+      it { expect { player_logger.print_pinfalls }.to output(pinfalls).to_stdout }
+    end
+
+    context 'all fouls' do
+      it { expect { all_fouls_logger.print_pinfalls }.to output(all_fouls_pinfalls).to_stdout }
+    end
   end
 
   describe '#print_scores' do
-    it { expect { logger.print_scores }.to output(scores).to_stdout }
+    context 'average game' do
+      it { expect { player_logger.print_scores }.to output(scores).to_stdout }
+    end
+
+    context 'all fouls' do
+      it { expect { all_fouls_logger.print_scores }.to output(all_fouls_scores).to_stdout }
+    end
   end
 
   describe '#print' do
-    it { expect { logger.print }
+    it { expect { player_logger.print }
          .to output("Jeff\n" + pinfalls + scores).to_stdout }
   end
 end
