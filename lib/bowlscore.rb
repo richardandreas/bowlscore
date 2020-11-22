9d13dc687f0d2b839a60942ff8e29e9ccbda6155
Richard Andreas Stierlin <@61023391341>
2020-11-21 22:34:07 -0300

    feat: printing table

--- lib/bowlscore.rb
+++ lib/bowlscore.rb
@@ -1,5 +1,6 @@
 require 'player_score'
 require 'score_table_logger'
+require 'helpers/bowlscore_helper'
 require 'file_parsers/bowlscore_file_parser'
 
 # For debugging
@@ -7,10 +8,21 @@ require 'byebug'
 
 # Main application class
 class Bowlscore
-  def self.start
-    puts 'Hello World!'
+  extend BowlscoreHelper
+
+  def self.start(file_path)
+    scores = BowlscoreFileParser.parse(file_path)
+    player_scores = group_by_player(scores)
+
+    ScoreTableLogger.print_header
+
+    player_scores.each do |player_name, scores|
+      player_score = PlayerScore.new(scores)
+
+      ScoreTableLogger.print(player_score, player_name)
+    end
   end
 end
 
 # Runs the main application
-Bowlscore.start
+Bowlscore.start('./spec/support/test-input-1.txt')
